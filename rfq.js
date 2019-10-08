f06b5108a083c86ab3ac4e082c5daf016ef25125
vikranteverx <56284531+vikranteverx@users.noreply.github.com>
2019-10-08 14:09:52 +1100

    Add files via upload

---
+++ rfq.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function() {
+
+	/*  FIELDS */
+
+	// Short Text
+	// jQuery('[data-id="49250411"] input').val('Ola');
+	jQuery('[data-id="56017663"] input').val('Ola');
+
+	// Textarea
+	
+	// jQuery('[data-id="49250408"] textarea').val('The quick brown fox jumps over the lazy dog');
+
+	// Single choice
+	
+	// jQuery('[data-id="49250413"] input[data-index=1]').click();
+
+	// Multiple choice
+	
+	// jQuery('[data-id="49250414"] input[data-index=0]').click();
+
+	// Dropdown
+	
+	// jQuery('[data-id="49250415"] select').val(jQuery('[data-id="49250415"] select option[data-index=2]').val()).trigger('change');
+
+	// Name (composed field)
+	
+	// jQuery('[data-id="49250505"] input[data-index=1]').val('John');
+	
+	// jQuery('[data-id="49250505"] input[data-index=2]').val('Doe');
+
+
+	/* BUTTONS */
+
+	// to emulate a Click on Submit, uncomment the next line:
+	// jQuery('[data-role="submit"]').focus().click();
+
+	// to emulate a Click on Next Page, uncomment the next line:
+	// jQuery('button[data-role="next-page"]').focus().click();
+
+	// to emulate a Click on Previous, uncomment the next line:
+	// jQuery('button[data-role="previous-page"]').focus().click();
+});
