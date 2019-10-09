19319cfd555e818b885a60194a2ec0da42ae0f51
vikranteverx <56284531+vikranteverx@users.noreply.github.com>
2019-10-09 15:41:46 +1100

    Update bq.js

--- bq.js
+++ bq.js
@@ -1,8 +1,8 @@
-jQuery(document).ready(function() {
+//jQuery(document).ready(function() {
 
 	
 	//var productUrl = window.location.href;
 	var productUrl = "Hello";
 	loader.engine.document.getElementById("57541314").setValue(({"value":  productUrl}));
 	
-});
+//});
