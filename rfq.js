46cba0c8c3ebeea58a49bb5deb7b4bb04d81dbab
vikranteverx <56284531+vikranteverx@users.noreply.github.com>
2019-10-09 11:43:05 +1100

    Update rfq.js

--- rfq.js
+++ rfq.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function() {
 
 	
 	var productUrl = window.location.href;
-	loader.engine.document.getElementById(57506942).setValue(({"value":  productUrl}));
+	loader.engine.document.getElementById("57506942").setValue(({"value":  productUrl}));
 	
 });
