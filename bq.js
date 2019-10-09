1c7098b3601890a2286d15b017a1fd666d48fff8
vikranteverx <56284531+vikranteverx@users.noreply.github.com>
2019-10-09 15:56:05 +1100

    Update bq.js

--- bq.js
+++ bq.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	
 	//var productUrl = window.location.href;
 	var productUrl = "Hello";
-	loader.engine.document.getElementById("57541314").setValue(({"value":  productUrl}));
+	//loader.engine.document.getElementById("57541314").setValue(({"value":  productUrl}));
+	document.getElementById('57541314').value = "100";
 	
 });
